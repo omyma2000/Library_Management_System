214dcd02581912a4eb8c89d97a898bd3750ce394
Omyma Boissa <omymaboissa@gmail.com>
2026-06-05 01:27:23 +0300

    oh - omaymahamad2

--- src/main/java/My_Classes/DB.java
+++ src/main/java/My_Classes/DB.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package My_Classes;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+
 
 /**
  *
@@ -10,6 +13,21 @@ package My_Classes;
  */
 public class DB {
     // connectios
-    
-    
-}
+    private static String serverName = "localhost";
+    private static String userName = "root";
+    private static String dbName= "java_library_system";
+    private static Integer portNumber = 3306 ;
+    private static String pass =""; // no password
+    // function to (create & retrun ) connection .
+    public static Connection getConnection (){
+       Connection connection = null;
+       MysqlDataSource dataSource = new MysqlDataSource();
+       dataSource.setServerName(serverName);
+       dataSource.setUser(userName);
+       dataSource.setDatabaseName(dbName);
+       dataSource.setPortNumber(portNumber);
+       dataSource.setPassword(pass);
+       return connection;
+    }
+    }
+
